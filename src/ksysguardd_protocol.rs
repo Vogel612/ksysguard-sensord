4880800b98b549a17808a6082fbc71199a2d2f52
Vogel612 <vogel612@gmx.de>
2022-05-01 16:15:44 +0200

    finally get communication between threads working

--- src/ksysguardd_protocol.rs
+++ src/ksysguardd_protocol.rs
@@ -1,20 +1,28 @@
 use crate::config::{ Config };
 use crate::monitor::{ Monitor, create as create_monitor };
 
-use tokio::net::{ TcpListener, TcpStream };
+// use tokio::net::{ TcpListener, TcpStream };
+use std::net::{ TcpListener, TcpStream };
 use std::collections::{ HashMap };
-use std::io::{ BufReader, BufWriter, BufRead };
 
+use std::io::{ BufReader, BufWriter, BufRead, Write };
+use either::{ Either };
+
+const UNKNOWN_COMMAND: &str = "UNKNOWN COMMAND";
 const MESSAGE_END: &str = "\nksysguardd> ";
 const DEFAULT_BIND_SPEC: &str = "0.0.0.0";
 
-struct Commands {
+enum Exit {
+    Exit,
+}
+
+struct Commands<'a> {
     // a monitor has multiple sensors?!
     //   (like "nvidia-smi dmon" contains multiple information points)
     // it maintains their metainformation (datatype, value range, ...)
     // for monitoring sensors it retains their latest value
     // for polling sensors it is responsible for getting their value
-    pub monitors: HashMap<&'static str, Box<dyn Monitor>>,
+    pub monitors: &'a HashMap<String, Box<dyn Monitor>>,
 }
 
 // required commands:
@@ -22,45 +30,111 @@ struct Commands {
 // a description for a sensor
 // recommended "commands":
 // test, specific sensors involve sensors
-impl Commands {
-    fn on_command(command: &str, reply_to: fn(&str) -> ()) {
+impl Commands<'_> {
+    fn on_command(&self, input: &str) -> Either<String, Exit> {
+        let command = input.trim();
         if command.eq_ignore_ascii_case("quit") {
             // terminate!
+            return either::Right(Exit::Exit);
         }
         if command.starts_with("test") {
+            // FIXME actually check whether the command is supported
+            return either::Left("0".into());
             // checks whether a given command is supported
             // current ksysguardd does not actually support this, soo ...
         }
         if command.starts_with("monitors") {
-            // list all monitors
+            return either::Left(itertools::join(self.monitors.keys(), "\n"));
+        }
+        let is_info_query = command.ends_with("?");
+        if is_info_query {
+            // currently unsupported
+        } else {
+            if self.monitors.contains_key(command) {
+                let mon = self.monitors.get(command).unwrap();
+                return either::Left(mon.get_value());
+            }
         }
         // attempt to find the monitor specified, give information if terminated by ?
         // return UNKNOWN COMMAND if no matching monitor is found
+        return either::Left(UNKNOWN_COMMAND.into());
     }
     // TODO "RECONFIGURE" over stderr??
+    fn handle_client(&self, socket: & TcpStream) {
+        // let sock = &socket.into_std().expect("Could not convert TcpStream to native socket");
+
+        let mut reader = BufReader::new(socket);
+        let mut writer = BufWriter::new(socket);
+        writer.write(MESSAGE_END.as_bytes())
+            .and(writer.flush());
+        let mut line_buf = String::new();
+        loop {
+            line_buf.clear();
+            let bytes = reader.read_line(&mut line_buf);
+            match bytes {
+                Ok(0) => {
+                    // EOF 
+                    break;
+                },
+                Ok(_) => {
+                    let result = self.on_command(&* line_buf);
+                    match result {
+                        either::Left(response) => {
+                            // compiler wants us to unwrap this and handle write errors
+                            writer.write(response.as_bytes())
+                                .and(writer.write(MESSAGE_END.as_bytes()))
+                                .and(writer.flush());
+                        },
+                        either::Right(_) => {
+                            // or alternatively exit?
+                            break;
+                        }
+                    }
+                },
+                Err(_e) => {
+                    // TODO handle error when reading from socket
+                }
+            }
+        }
+    }
 }
 
 pub struct Sensord {}
 
 impl Sensord {
-    pub async fn start(config: &'static Config) {
-
-        let mut known_monitors: HashMap<&str, Box<dyn Monitor>> = HashMap::new();
+    pub fn start(config: &Config) {
+        let mut known_monitors = HashMap::new();
         for monitor_definition in config.monitors.iter() {
-            known_monitors.insert(&*monitor_definition.name, create_monitor(&monitor_definition));
+            known_monitors.insert(monitor_definition.name.to_owned(), create_monitor(&monitor_definition));
         }
 
         // establish listening socket
-        let listener = TcpListener::bind((DEFAULT_BIND_SPEC, config.port)).await
+        let listener = TcpListener::bind((DEFAULT_BIND_SPEC, config.port))
             .expect("Failed to bind listening port to accept connections.");
         let protocol = Commands {
-            monitors: known_monitors,
+            monitors: &known_monitors,
         };
+        let mut join_handles: Vec<std::thread::JoinHandle<()>> = Vec::new();
+        for mon in known_monitors.values() {
+            match mon.start() {
+                Some(handle) => { join_handles.push(handle) }
+                None => {}
+            }
+        }
         loop {
-            // let (socket, _) = listener.accept().await?;
+            match listener.accept() {
+                Ok((socket, _)) => {
+                    protocol.handle_client(&socket);
+                    break;
+                },
+                Err(_e) => {
+                    println!("Failed to accept a connection to the TCP listen port!");
+                }
+            }
         }
 
-        // start sensor data collection (for monitor sensors)
-        // register polling sensors
+        for h in join_handles {
+            h.join();
+        }
     }
 }
--- src/monitor.rs
+++ src/monitor.rs
@@ -4,20 +4,22 @@ use std::process::{ Command, Stdio };
 
 use std::io::{ BufReader, BufRead };
 
+use std::sync::{ RwLock, Arc };
+
 pub trait Monitor {
     fn name(&self) -> &str;
     fn defined_command(&self) -> &str;
     fn get_value(&self) -> String;
+    fn start(&self) -> Option<std::thread::JoinHandle<()>>;
 }
 
 pub fn create(definition: &MonitorDefinition) -> Box<dyn Monitor> {
     if definition.kind == MonitorKind::Listening {
         let lm = ListeningMonitor {
             name: definition.name.to_owned(),
-            current_value: String::new(),
             defined_command: definition.command.to_owned(),
+            current_value: Arc::new(RwLock::new(String::new())),
         };
-        lm.start();
         return Box::new(lm);
     } else {
         let pm = PollingMonitor {
@@ -36,18 +38,44 @@ pub struct PollingMonitor {
 pub struct ListeningMonitor {
     pub name: String,
     pub defined_command: String,
-    pub current_value: String,
+    pub current_value: Arc<RwLock<String>>,
 }
 
-impl ListeningMonitor {
-    pub fn start(mut self) -> std::thread::JoinHandle<()> {
+impl Monitor for PollingMonitor {
+    fn name(&self) -> &str { todo!() }
+    fn defined_command(&self) -> &str { todo!() }
+    fn get_value(&self) -> String {
+        let output = Command::new(&*self.defined_command)
+            .stdout(Stdio::piped())
+            .output()
+            .expect(&*format!("Could not start subprocess for monitor {}.", self.name));
+
+        return String::from_utf8(output.stdout)
+            .expect(&*format!("Child process output for monitor {} was not parseable as UTF-8", self.name));
+    }
+    fn start(&self) -> std::option::Option<std::thread::JoinHandle<()>> {
+        return Option::None;
+    }
+}
+
+impl Monitor for ListeningMonitor {
+    fn name(&self) -> &str { &*self.name }
+    fn defined_command(&self) -> &str { &*self.defined_command }
+    fn get_value(&self) -> String {
+        let read_guard = self.current_value.read().unwrap();
+        return read_guard.to_owned();
+    }
+
+    fn start(&self) -> Option<std::thread::JoinHandle<()>> {
         let mut child = Command::new(&*self.defined_command)
             .stdout(Stdio::piped())
             .spawn()
             .expect(&*format!("Could not start subprocess for monitor {}.", self.name));
             
         let stdout = child.stdout.take().unwrap();
-        
+
+        let clone = Arc::clone(&self.current_value);
+
         let handle = std::thread::spawn(move || {
             let mut reader = BufReader::new(stdout);
             let buffer = &mut String::new();
@@ -63,29 +91,14 @@ impl ListeningMonitor {
                         // not sure what exactly we can do here
                     }
                 }
-                self.current_value = buffer.to_string();
+                {
+                    // explicit block to make me feel better about not being able to explicitly call guard.drop()
+                    let mut guard = clone.write().unwrap();
+                    *guard = buffer.to_owned();
+                    // guard.drop();
+                }
             }
         });
-        return handle;
+        return Option::Some(handle);
     }
-}
-
-impl Monitor for PollingMonitor {
-    fn name(&self) -> &str { todo!() }
-    fn defined_command(&self) -> &str { todo!() }
-    fn get_value(&self) -> String { 
-        let output = Command::new(&*self.defined_command)
-            .stdout(Stdio::piped())
-            .output()
-            .expect(&*format!("Could not start subprocess for monitor {}.", self.name));
-
-        return String::from_utf8(output.stdout)
-            .expect(&*format!("Child process output for monitor {} was not parseable as UTF-8", self.name));
-    }
-}
-
-impl Monitor for ListeningMonitor {
-    fn name(&self) -> &str { &*self.name }
-    fn defined_command(&self) -> &str { &*self.defined_command }
-    fn get_value(&self) -> String { self.current_value.to_owned() }
 }
